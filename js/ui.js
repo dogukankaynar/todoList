25f948be4d1bcfd9bf729c2717b625d79755e0ed
dogukankaynar <dogukankaynar98@gmail.com>
2022-04-16 20:15:12 +0300

    ui.js de addTodoUı hatasını düzelt

--- js/ui.js
+++ js/ui.js
@@ -5,11 +5,9 @@ UI.prototype.clearInputs = (todoTitle, text) => {
 };
 UI.prototype.addTodoUı = (newTodo) => {
   const todoList = document.querySelector(".todo-menu");
-  const todoMessage=document.querySelector(".todo-message");
   todoList.innerHTML += `
     <span class='new-todo-title'><p>${newTodo.title}</p><a href="#"><img id='delete' src="https://img.icons8.com/material-sharp/24/000000/delete-trash.png"/></a></span>
      `;
-     todoMessage.innerHTML=`<span><p>${newTodo.todoText}</p></span>`
 }
 UI.prototype.loadAllTodos = (todos) => {
   const todoList = document.querySelector(".todo-menu");
